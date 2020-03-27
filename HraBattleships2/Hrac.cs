fadd06bcd4346605c6cdeb3d1e777ab6f5f5ab7c
petr.jechumtal <petr.jechumtal@seznam.cz>
2020-03-27 17:36:17 +0100

    Position

--- HraBattleships2/Hrac.cs
+++ HraBattleships2/Hrac.cs
@@ -21,14 +21,29 @@ namespace HraBattleships2
             width = gameSettings.BoardWidth;
 
             var shipPositions = new List<ShipPosition>();
-            byte y = 1;
-            foreach (var shipType in gameSettings.ShipTypes.OrderByDescending(x => (int)x))
+
+            var positionSubmarine = new Position(2, 3);
+            shipPositions.Add(new ShipPosition(ShipType.Submarine,positionSubmarine, Orientation.Right));
+            
+            var positionDestroyer = new Position(13, 5);
+            shipPositions.Add(new ShipPosition(ShipType.Destroyer, positionDestroyer, Orientation.Right));
+
+            var positionCruiser = new Position(5, 6);
+            shipPositions.Add(new ShipPosition(ShipType.Cruiser, positionCruiser, Orientation.Down));
+
+            var positionBattleship = new Position(9, 11);
+            shipPositions.Add(new ShipPosition(ShipType.Battleship, positionBattleship, Orientation.Down));
+
+            var positionCarrier = new Position(2,13 );
+            shipPositions.Add(new ShipPosition(ShipType.Carrier, positionCarrier, Orientation.Right));
+
+            /*foreach (var shipType in gameSettings.ShipTypes.OrderByDescending(x => (int)x))
             {
                 var position = new Position(1, y);
                 shipPositions.Add(new ShipPosition(shipType, position, Orientation.Right));
                 y += 2;
             }
-
+            */
             return shipPositions.ToArray();
 
         }
